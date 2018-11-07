e4a9e369ff56fb5d72cf5a076549d61710ab69f0
Lorenz Nunag <lorenznunag@gmail.com>
2018-11-07 11:28:58 -0500

    four buttons done

--- JS/js.js
+++ JS/js.js
@@ -1,3 +1,14 @@
-function sayHello() {
-  console.log("Hello Console");
+function sayConsole() {
+  console.log("Hello, World!");
+}
+function sayPopup() {
+  alert("Hello, World!");
+}
+function sayBrowser() {
+		var p = document.getElementById("Hello");
+    p.innerHTML = "Hello, World!";
+}
+function sayStranger() {
+    var person = prompt("Please enter your name");
+    alert("Hello, " + person + "!");
 }
